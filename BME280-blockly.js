fd66e633cae7d8d7cd56d42693c57b9d226c4d45
sky <sky@wogogo.com>
2018-01-05 15:52:27 +0800

    [修改] 大小寫

--- BME280-blockly.js
+++ BME280-blockly.js
@@ -3,7 +3,7 @@
     'use strict';
   
     window.getBME280= function (board, pin) {
-      return new webduino.module.bme280(board, board.getDigitalPin(pin));
+      return new webduino.module.BME280(board, board.getDigitalPin(pin));
     };
   
   }(window, window.webduino));
